4fdafc9ff3ea7f709cc5c2f6615668abb0552efe
SergeDmi <dmitrief@Serges-MacBook-Pro.local>
2018-02-04 23:21:05 +0100

    now working

--- statistics/multidim_orthogonal.m
+++ statistics/multidim_orthogonal.m
@@ -1,5 +1,5 @@
-function [ linco, offset,  Rsq , hist] = multidim_orthogonal( Y,X,nimp )
-% [ linco, offset, res ] = multidim_orthognal( Y,X,nimp )
+function [ lincoeffs, offset,  Rsq , hist] = multidim_orthogonal( Y,X,nimp )
+% [ lincoeffs, offset, res ] = multidim_orthognal( Y,X,nimp )
 %   USES PCA to do a robus regression of Y from nimp columns in X
 % 
 %
@@ -7,16 +7,17 @@ function [ linco, offset,  Rsq , hist] = multidim_orthogonal( Y,X,nimp )
 % X are the parameters (matrix N_experiments x N_params)
 % nimp is the number of predictors to be taken from N_params
 %
-% linco are the linear coefficients
+% lincoeffs are the linear coefficients
 % note that they are of the form ( Coeff1 Coeff2 Coeff3 ... )
 % offset is the constant offset
 % hist is the order of parameters used for regression
 % Rsq is the Rsq value after N steps
 % 
 % Not suitable for anything, really
-% Serge Dmitrieff
+% Serge Dmitrieff 2018
 % www.biophysics.fr
 
+%% Checking input
 S=size(Y);
 if min(S)>1
     error('Now limited to 1 measured output')
@@ -33,60 +34,43 @@ if S(2)==ny
     S=S(2:-1:1);
 end
 nx=S(2)+1;
-[X,CX]=nono(X);
-X=[Y X];
-
+[XX,CX]=nono(X);
+XX=[Y XX];
+varY=var(Y);
 
 %% Preparing the data
 chosable=ones(1,nx);
 chosable(1)=0;
-%factors=ones(1,nx);
-%offsets=ones(1,nx);
-
-for i=2:nx
-	rangex=max(X(:,i))-min(X(:,i));
-    if rangex==0
-        chosable(i)=0;
-		%else
-    end
-end
-
+chosable(2:end)=CX>0;
 
 nvx=sum(chosable);
 bli=1:nx;
-%choices=bli(chosable);
         
 if nimp>nvx
     nimp=nvx;
     disp('Warning : changing predictor number to variable number')
 end       
 
-
-
 left_ix=logical(chosable);
 left=bli(left_ix);
 hist=[1];
 res=zeros(1,nimp);
 
-
-
 %% Simple method
-% Removing mean 
 
-% We find the best to worse predictor in X
+% We find important variables in order
 for ni=1:nimp
     scores=ones(1,nvx);
     
-    
+    % We find the best to worse predictor in X
     for j=1:sum(left_ix)
         vec=[hist left(j)];
-        M=X(:,vec);
+        M=XX(:,vec);
         %coefs=M\YY;
 		[~,~,latent]=princom(M);
         scores(j)=latent(end);
         %scores(j)=-sum(sc2);
     end
-    
     [~,rk]=min(scores);
     ix=left(rk);
     % The next best predictor is #ix
@@ -99,27 +83,36 @@ for ni=1:nimp
     nvx=sum(left_ix);
 end
     
- M=X(:,vec);
+ M=XX(:,hist);
+ % Back to computing CPA
 [coefs]=princom(M);
-
-
+% Computing normal vector to main plane and getting linear coeffcients
+%linco=-(xprod(2:end)./CX(hist)')/xprod(1);
 [xprod]=ndim_xprod(coefs(:,1:ni));
-linco=-(xprod(2:end)./CX(:))/xprod(1);
+linco=-xprod(2:end)/xprod(1);
+% Back to real coefficients by inverting normalization
+hist=(hist(2:end)-1);
+linco=linco'./CX(hist);
+lincoeffs=zeros(1,nx-1);
+lincoeffs(hist)=linco;
+errs=Y-sum((ones(ny,1)*(lincoeffs)).*X,2);
+offset=mean(errs);
+%Rsq(nimp)=1-(devY-std(errs))/devY;
+% Computing the Rsquared
+Rsq=(varY-var(errs))/varY;
 
-%coefs(:,1:ni)
-	
-%linco=coefs(2:end);
-offset=coefs(1);
-Rsq=1-res;
-hist=hist(2:end)-1;
+%rest=mean(Y)-sum(mean(X,1).*linco);
+%offset=coefs(1);
+
 
 end
 
 function [X,cof] = nono(X)
+    % Normalizing input data :
+    % centering on zero and dividing by deviation
     s=size(X);
     b=ones(s(1),1);
-    size(b*mean(X,1))
     X=X-b*mean(X,1);
-    cof=var(X,1);
+    cof=std(X,1);
     X=X./(b*cof);
 end
