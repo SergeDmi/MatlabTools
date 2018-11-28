4b4a0fa51905d93366d721e7f322d88082fb8061
SergeDmi <serge.dmitrieff@gmail.com>
2018-11-28 17:18:57 +0100

    improved rolling average

--- statistics/rolling_average.m
+++ statistics/rolling_average.m
@@ -1,28 +1,57 @@
-function [ yfit ] = rolling_average( Y,X,xfit,dist )
+function [ y_fit , y_std ] = rolling_average( Y,X,dist,x_fit)
 %ROLLING_AVERAGE Performs a rolling average
-%   Gives yfit corresponding to bins xfit from data Y,X 
+%   Gives y_fit corresponding to bins x_fit from data Y,X 
 %   with a haracteristic distance dist
 %   for now only gaussian weight
+%
+% INPUT
+%   X is a (nf * 1) vector of coordinates that will be rolled on
+%   Y is an (nf * ny ) array of observables to be be averaged
+%   dist is the rolling distance
+%   xfit is the fitted coordinates ; takes X if empty.
+%
+% OUTPUT
+%   y_fit is the averaged Y
+%   y_std is the standard deviation
+%
+% @TODO : add function handle as input parameter
 %
 % Serge Dmitrieff, Institut Jacques Monod, 2018
 % www.biophysics.fr
 
+if nargin<4
+	x_fit=X;
+end
+
 % First we put data in the correct format
 sX=length(X);
-nf=length(xfit);
-xfit=reshape(xfit,1,nf);
+nf=length(x_fit);
+x_fit=reshape(x_fit,1,nf);
+sY=size(Y);
 X=reshape(X,sX,1);
-Y=reshape(Y,sX,1);
+if sY(2)==sX
+	Y=Y';
+	sY=size(Y);
+end
 
-% Making matrixes rather than loops
+nY=sY(2);
+y_fit=zeros(nf,nY);
+y_std=zeros(nf,nY);
+
 ro=ones(1,nf);
 XX=X*ro;
-YY=Y*ro;
+col=ones(sX,1);
+weights=exp(-((XX-col*x_fit).^2)./(2*dist*dist));
 
-% 3,2,1, let's jam
-weights=exp(-((XX-ones(sX,1)*xfit).^2)./(2*dist*dist));
-yfit=sum( (YY.*weights),1 )./sum( weights,1);
-
+for i=1:nY
+	% Making matrixes rather than loops
+	YY=Y(:,i)*ro;
+	% 3,2,1, let's jam
+	y_fit(:,i)=sum( (YY.*weights),1 )./sum( weights,1);
+	
+	y_std(:,i)=sqrt(sum( ((YY-col*(y_fit(:,i)')).^2).*weights,1)./sum(weights,1)); 
+	
+end
 % That was fast !
 
 end
